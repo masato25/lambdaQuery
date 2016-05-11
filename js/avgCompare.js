fc524b81d13a391b8e827ca2773a0ad33a572d4a
masato25 <leomasato25@gmail.com>
2016-05-11 23:59:09 +0800

    update lambda funcations

--- js/avgCompare.js
+++ js/avgCompare.js
@@ -1,13 +1,17 @@
-limit = (typeof limit == "undefined"? 3 : limit)
+avg = 0
 t2 = _.map(input, function(res){
   res.Avg = _.reduce(res.Values, function(sum,v){
     return (sum+v.Value)
   },0) / (res.Values.length === 0 ? 1 : res.Values.length)
+  avg += res.Avg
   return res;
 })
 
-t3 = _.chain(t2).sortBy(function(res){
-  return - res.Avg;
-}).first(limit).value();
-
+avg = avg/t2.length
+console.log("current avg number: " + avg)
+t3 = _.filter(t2, function(x){
+  if(x.Avg > avg){
+    return x.Endpoint, x.Avg
+  }
+})
 output = JSON.stringify(t3)
